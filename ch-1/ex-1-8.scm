62a45c65461cd6478675ef25192467e08aec2cc1
cgombauld <clement.gombauld@cpexterne.org>
2023-11-20 08:21:10 +0100

    solve 1-7 chapter 1

--- ch-1/ex-1-8.scm
+++ ch-1/ex-1-8.scm
@@ -8,8 +8,6 @@
 (define (average x y)
  (/ (+ x y) 2))
 
-(define (improve guess x ) 
-(average guess (/ x guess)))
 
 (define (improve-newton guess x) 
 (/ (+ 
@@ -20,18 +18,12 @@
 (define (good-enough? guess x)
 ( < (abs (- (square guess) x) ) 0.001))
 
-(define (sqrt-iter guess x)
-(if (good-enough? guess x)
-guess
-(sqrt-iter (improve guess x) x)))
 
 (define (sqrt-iter-newton guess x)
 (if (good-enough? guess x)
 guess
 (sqrt-iter (improve-newton guess x) x)))
 
-(define (sqrt x) 
-(sqrt-iter 1.0 x))
 
 (define (sqrt-newton x)
 (sqrt-iter-newton 1.0 x))
