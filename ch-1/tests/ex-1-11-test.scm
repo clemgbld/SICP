0d4b515cac8a9c38cfb5dcc8859cade724347704
cgombauld <clement.gombauld@cpexterne.org>
2023-12-01 08:05:16 +0100

    properly test drive f

--- ch-1/tests/ex-1-11-test.scm
+++ ch-1/tests/ex-1-11-test.scm
@@ -6,6 +6,8 @@
    (check (= 2 (f 2)) "sould be n when n less than 3")
    (check (= 4 (f 3)) "sould be 4 when n is 3")
    (check (= 11 (f 4)) "sould be 11 when n is 4")
+   (check (= 25 (f 5)) "sould be 25 when n is 5")
+   (check (= 59 (f 6)) "sould be 59 when n is 6")
 )
 
 (run-registered-tests)
