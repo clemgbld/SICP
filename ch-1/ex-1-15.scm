28ae0f543aa5eac44279bbed00316472479804d0
cgombauld <clement.gombauld@cpexterne.org>
2023-12-03 19:56:44 +0100

    solve exercise 1.15

---
+++ ch-1/ex-1-15.scm
@@ -0,0 +1,14 @@
+(define (cube x) (* x x x))
+
+(define (p x) (- (* 3 x) (* 4 (cube x)))) 
+
+(define (sine angle)
+(if (not (> (abs angle) 0.1)) angle
+(p (sine (/ angle 3.0)))))
+
+(sine 12.15)
+;p is called 4 times
+
+;2) What is the orders of growth of the steps ? O(log(n))
+
+;3) What is the orders of growth of the space ? O(log(n))
