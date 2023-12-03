6b9fa9055214798fb5a6a41d0faeb05666de5472
cgombauld <clement.gombauld@cpexterne.org>
2023-12-03 12:08:20 +0100

    solve ex 12 of chapter 1

--- ch-1/ex-1-12.scm
+++ ch-1/ex-1-12.scm
@@ -1,4 +1,14 @@
 (define (pascal c r) 
-(cond ((> c r) #f) 
-((or (= c 1) (= c r)) 1)
-(else 2)))
+(cond ((is-out-of-bound c r) #f) 
+((is-edge c r) 1)
+(else (+ (pascal (dec c) (dec r)) 
+(pascal c (dec r))))))
+
+(define (is-out-of-bound c r) 
+(or (> c r) (< c 1)))
+
+(define (is-edge c r) 
+(or (= c 1) (= c r)))
+
+(define (dec n) 
+(- n 1))
