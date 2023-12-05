f9b9b34c6d37c1362bd3e97f94eb16004654068e
cgombauld <clement.gombauld@cpexterne.org>
2023-12-05 12:15:03 +0100

    solve ex 1.17

---
+++ ch-1/ex-1-17.scm
@@ -0,0 +1,10 @@
+(define (multiply a b) (cond ((= b 0) 0)
+      ((is-even? b) (double (multiply a (/ b 2))))
+      (else (+ a (multiply a (- b 1))))))
+
+
+(define (is-even? n)
+  (= (remainder n 2) 0))
+
+(define (double n) 
+(+ n n))
---
+++ ch-1/tests/ex-1-17-test.scm
@@ -0,0 +1,16 @@
+(load "test-manager/load.scm")
+(load "ch-1/ex-1-17.scm")
+
+
+(define-each-test
+(check (= 4 (multiply 2 2)) "2 times 2 should b 4")
+(check (= 25 (multiply 5 5)) "5 times 5 should b 5")
+(check (= 72 (multiply 8 9)) "8 times 9 should b 72")
+(check (= 81 (multiply 9 9)) "9 times 9 should b 81")
+)
+
+
+
+
+
+(run-registered-tests)
