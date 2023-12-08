069b786300cfecfb95ad78f0aec755a7305872ad
cgombauld <clement.gombauld@cpexterne.org>
2023-12-08 21:24:50 +0100

    solve ex 1-23

---
+++ ch-1/tests/ex-1-23.test.scm
@@ -0,0 +1,14 @@
+(load "test-manager/load.scm")
+(load "ch-1/ex-1-23.scm")
+
+(define-each-test
+(check (= 2 (find-divisor 2 2)) "should be 2")
+(check (= 3 (find-divisor 9 3)) "should be 3"))
+
+(define-each-test
+(check (= 3 (next 2)) "should be 3")
+(check (= 5 (next 3)) "should be 5")
+)
+
+
+(run-registered-tests)
