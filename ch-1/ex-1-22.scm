e1733f757e24417e60e40d8d66fd469485e75697
cgombauld <clement.gombauld@cpexterne.org>
2023-12-08 21:11:32 +0100

    solve ex-1-22

--- ch-1/ex-1-22.scm
+++ ch-1/ex-1-22.scm
@@ -1,27 +1,42 @@
-(define (timed-prime-test n) 
-(newline)
-(display n)
-(start-prime-test n (system-clock)))
+(define (smallest-divisor n)
+  (find-divisor n 2))
 
-(define (start-prime-test n start-time) 
-(if (prime? n)
-(report-prime (- (system-clock) start-time)))) 
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n)
+         n)
+        ((divides? test-divisor n)
+         test-divisor)
+        (else (find-divisor
+               n
+               (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))
+      "nothing"))
 
 (define (report-prime elapsed-time)
   (display " *** ")
   (display elapsed-time))
 
-(define (prime? n)
-(= n (smallest-divisor n)))
+(define (search-for-primes start-range end-range)
+  (if (even? start-range)
+      (search-for-primes (+ 1 start-range) end-range)
+      (cond ((> start-range end-range)
+             (newline) (display "done"))
+            (else (timed-prime-test start-range)
+                  (search-for-primes (+ 2 start-range) end-range)))))
 
-(define (smallest-divisor n) (find-divisor n 2))
-
-(define (find-divisor n test-divisor) 
-(cond ((> (square test-divisor) n) n)
-((divides? test-divisor n) test-divisor)
-(else (find-divisor n (+ test-divisor 1)))))
-
-(define (divides? a b) (= (remainder b a) 0))
-
-
-(timed-prime-test 2745381571)
+(search-for-primes 10000000000 10000000090)
