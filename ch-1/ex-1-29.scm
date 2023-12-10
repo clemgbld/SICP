02fa37261ee5315d1728953cd5042f8c82e5bc17
cgombauld <clement.gombauld@cpexterne.org>
2023-12-10 18:23:37 +0100

    solve ex 1 29

---
+++ ch-1/ex-1-29.scm
@@ -0,0 +1,23 @@
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
+
+(define (integral-simpson f a b n)
+  (define h (/ (- b a) n))
+  (define (add-2h x) (+ x h h))
+  (* (+ (f a)
+        (* 2 (sum f a add-2h b))
+        (* 4 (sum f (+ a h) add-2h b))
+        (f b))
+     (/ h 3)))
+
+(define (cube x) (* x x x))
+
+
+(define (sum term a next b) 
+(if (> a b) 0
+(+ (term a)
+   (sum term (next a) next b))))
