7ff123c6af3065fd42eba3beff11f558f5eeb58d
cgombauld <clement.gombauld@cpexterne.org>
2023-12-16 17:35:37 +0100

    refacto with O(log n) solution for 1 43

--- ch-1/ex-1-43.scm
+++ ch-1/ex-1-43.scm
@@ -3,4 +3,5 @@
 (define (repeated f n)
   (cond ((< n 1) identity)
     ((= n 1) f)
-      (else (compose f (repeated f (- n 1))))))
+      ((even? n) (repeated (compose f f) (/ n 2))) 
+        (else (compose f (repeated f (- n 1))))))
--- ch-1/tests/ex-1-43.test.scm
+++ ch-1/tests/ex-1-43.test.scm
@@ -6,8 +6,7 @@
 (check (= 5   ((repeated square 0) 5))  "should square zero time")
 (check (= 25   ((repeated square 1) 5))  "should square 1 time")
 (check (= 625   ((repeated square 2) 5))  "should square 2 times")
-(check (= 390625   ((repeated square 3) 5))  "should square 3 times")
-)
+(check (= 390625   ((repeated square 3) 5))  "should square 3 times"))
 
 
 
