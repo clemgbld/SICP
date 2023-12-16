c405476405d5618f05541e22044f1d99a2f097c3
cgombauld <clement.gombauld@cpexterne.org>
2023-12-16 15:11:31 +0100

    fix functions.scm file

--- ch-1/tests/ex-1-43.test.scm
+++ ch-1/tests/ex-1-43.test.scm
@@ -1,11 +1,10 @@
 (load "test-manager/load.scm")
-(load "ch-1/ex-1-42.scm")
+(load "ch-1/ex-1-43.scm")
 
 
 (define-each-test
  (check ( = 1 (square-then-inc 0))  "should be 2")
   (check ( = 49 (square-then-inc 6))  "should be 49")
-  (check ( = 49 (square-then-inc-with-pipe 6))  "should be 49")
 )
 
 
--- functions/functions.scm
+++ functions/functions.scm
@@ -1,8 +1,8 @@
 (define (inc n) 
-(n + 1))
+(+ n 1))
 
 (define (dec n) 
-(n - 1))
+(- n 1))
 
 (define (double n) 
 (+ n n))
