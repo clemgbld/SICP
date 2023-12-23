3e3ac2fb827f893d0b2f2a4caa1c948b33b64baa
cgombauld <clement.gombauld@cpexterne.org>
2023-12-23 14:40:11 +0100

    fix ex 2.5

--- ch-2/ex-2-5.scm
+++ ch-2/ex-2-5.scm
@@ -1,9 +1,16 @@
-(define (cons-int x y) 
-    (lambda (num) (if (= num 0) (expt 2 x)
-        (expt 3 y))))
+(define (cons-int a b)
+  (* (expt 2 a) (expt 3 b)))
 
-(define (car-int dispatch) 
-    (dispatch 0))
+ (define (divides? a b) 
+   (= 0 (remainder b a))) 
+  
+ (define (count-0-remainder-divisions n divisor) 
+   (define (iter x divisions) 
+     (if (divides? divisor x) 
+         (iter (/ x divisor) (+ divisions 1)) 
+         divisions)) 
+   (iter n 0)) 
 
-(define (cdr-int dispatch) 
-    (dispatch 1))
+
+ (define (car-int z) (count-0-remainder-divisions z 2)) 
+ (define (cdr-int z) (count-0-remainder-divisions z 3)) 
--- ch-2/tests/ex-2-5.test.scm
+++ ch-2/tests/ex-2-5.test.scm
@@ -4,8 +4,8 @@
 
 
 (define-each-test 
-(check (= 4 (car-int (cons-int 2 2))) "should get the first integer apply as exponent to 2")
-(check (= 27 (cdr-int (cons-int 2 3))) "should get the second integer apply as exponent to 3"))
+(check (= 2 (car-int (cons-int 2 2))) "should get the first integer apply as exponent to 2")
+(check (= 3 (cdr-int (cons-int 2 3))) "should get the second integer apply as exponent to 3"))
 
 
 
