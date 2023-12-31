b56439130f9f192fc2069186effab23c54f8f3e0
cgombauld <clement.gombauld@cpexterne.org>
2023-12-31 18:36:12 +0100

    fix exercise 18

--- ch-2/tests/ex-2-18.test.scm
+++ ch-2/tests/ex-2-18.test.scm
@@ -5,6 +5,7 @@
 (check (equal? '() (rev '())) "should still be an empty list when there is nothing to reverse")
 (check (equal? '(1) (rev '(1))) "should still be the same list when there is only one element in the list")
 (check (equal? '(3 2 1) (rev '(1 2 3))) "should reverse the list")
+(check (equal? '(25 9 16 4 1) (rev '(1 4 16 9 25))) "should reverse the list")
 )
 
 (run-registered-tests)
