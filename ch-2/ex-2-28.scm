4f07f891328a8885b375d81d338c7d14ef5393b9
cgombauld <clement.gombauld@cpexterne.org>
2024-01-04 08:47:30 +0100

    fix ex 28 to handle tricky cases

--- ch-2/ex-2-28.scm
+++ ch-2/ex-2-28.scm
@@ -1,6 +1,13 @@
 (define (fringe tree)
     (if (null? tree) '() 
         (append (let ((node (car tree)))
-            (if (pair? (car node)) (fringe node)
-                node)) 
-                    (fringe (cdr tree)))))
+            (cond ((not (list? node)) (list node)) 
+                ((contains-list node) (fringe node))
+                    (else node))) 
+                        (fringe (cdr tree)))))
+
+(define (contains-list node) 
+    (cond ((null? node) #f)
+                ((list? (car node)) #t)
+                    (else (contains-list (cdr node)))))
+                        
--- ch-2/tests/ex-2-28.test.scm
+++ ch-2/tests/ex-2-28.test.scm
@@ -6,6 +6,8 @@
 (check (equal? (list 1 2)  (fringe (list (list 1 2))))  "should be the first list")
 (check (equal? (list 1 2 3 4)  (fringe (list (list 1 2) (list 3 4))))  "should flatten the list one level down")
 (check (equal? (list 1 2 3 4 1 2 3 4)  (fringe (list (list (list 1 2) (list 3 4)) (list (list 1 2) (list 3 4)))))  "should flatten the list multiple levels down")
+(check (equal? (list 1)  (fringe (list (list (list (list (list (list 1))))))))  "should flatten the list multiple levels down")
+(check (equal? (list 1 2 3 19 283 38 2 3 2 2 3 217 382 1827 2 187 2838 2 1 2 2 3 3 23 2 1 238)   (fringe (list (list (list 1 2 3 19 283 38) 2 3 2) (list 2 3 (list 217 382 1827) 2 187 (list 2838)) 2 1 2 (list 2 (list 3 (list 3)) 23 2 1 238))))  "should pass the acceptance test")
 )
 
 
