d05fce05c72f46608481ffad68487692313e5464
cgombauld <clement.gombauld@cpexterne.org>
2024-01-08 08:43:32 +0100

    solve exercise 37 of chapter 2

--- ch-2/ex-2-37.scm
+++ ch-2/ex-2-37.scm
@@ -1,4 +1,5 @@
 (load "functions/functions.scm")
+(load "ch-2/ex-2-36.scm")
 
 (define (dot-product v w)
       (accumulate + 0 (map * v w)))
@@ -6,3 +7,10 @@
 (define (matrix-*-vector m v) 
     (map (lambda (v-from-matrix) 
         (dot-product v-from-matrix v ))  m))
+
+(define (transpose mat) 
+    (accumulate-n cons '() mat))
+
+(define (matrix-*-matrix m n) 
+    (let ((cols (transpose n)))
+        (map (lambda (v) (matrix-*-vector cols v)) m)))
--- ch-2/tests/ex-2-37.test.scm
+++ ch-2/tests/ex-2-37.test.scm
@@ -5,6 +5,8 @@
 
 (define-each-test
 (check (equal? (list 13 31) (matrix-*-vector (list (list 1 2 3) (list 4 5 6)) (list 2 1 3))) "should be the product of the vector and the matrix")
+(check (deep-equal? (list (list 1 4) (list 2 5) (list 3 6)) (transpose (list (list 1 2 3) (list 4 5 6)))) "should transpose the matrix")
+(check (deep-equal? (list (list 19 22) (list 43 50) ) (matrix-*-matrix (list (list 1 2) (list 3 4)) (list (list 5 6) (list 7 8)))) "should multiply the first matrix with the second matrix")
 )
 
 
