1af66e418338a382710cd933aa4ccf57b9b185a4
cgombauld <clement.gombauld@cpexterne.org>
2024-01-14 18:44:45 +0100

    solve ex 51 of chapter 2

---
+++ ch-2/ex-2-51.scm
@@ -0,0 +1,34 @@
+(load "ch-2/ex-2-50.scm")
+
+(define (beside painter1 painter2)
+(let ((split-point (make-vect 0.5 0.0)))
+(let ((paint-left (transform-painter
+            painter1
+            (make-vect 0.0 0.0)
+            split-point
+            (make-vect 0.0 1.0)))
+          (paint-right
+           (transform-painter
+            painter2
+            split-point
+            (make-vect 1.0 0.0)
+            (make-vect 0.5 1.0))))
+(lambda (frame) (paint-left frame) (paint-right frame)))))
+
+(define (below painter1 painter2)
+(let ((split-point (make-vect 0.0 0.5)))
+(let ((paint-left (transform-painter
+            painter1
+            (make-vect 0.0 0.0)
+            (make-vect  1.0 0.0)
+            split-point))
+          (paint-right
+           (transform-painter
+            painter2
+            split-point
+            (make-vect 1.0 0.5)
+            (make-vect 0.0 1.0))))
+(lambda (frame) (paint-left frame) (paint-right frame)))))
+
+(define (below-bis painter1 painter2)
+    (rotate270 (beside (rotate90 painter1) (rotate90 painter2))))
