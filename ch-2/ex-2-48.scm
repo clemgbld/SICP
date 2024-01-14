19cd40e3f058a2d222ea870a8bd0135361bab992
cgombauld <clement.gombauld@cpexterne.org>
2024-01-14 16:40:26 +0100

    rollback ex 48

--- ch-2/ex-2-48.scm
+++ ch-2/ex-2-48.scm
@@ -1,7 +1,7 @@
 (load "ch-2/ex-2-46.scm")
 
 (define (make-segment start end)
-    (cons (make-vect 0 start) (make-vect 0 end)))
+    (cons start end))
 
 (define (start-segment s)
     (car s))
--- ch-2/tests/ex-2-48.test.scm
+++ ch-2/tests/ex-2-48.test.scm
@@ -2,8 +2,8 @@
 (load "ch-2/ex-2-48.scm")
 
 (define-each-test 
-(check (equal? (make-vect 0 2) (start-segment (make-segment 2 5))) "should select the start vector of the segment")
-(check (equal? (make-vect 0 3) (end-segment (make-segment 1 3))) "should select the start vector of the segment")
+(check (equal? (make-vect 0 2) (start-segment (make-segment (make-vect 0 2) (make-vect 1 3)))) "should select the start vector of the segment")
+(check (equal? (make-vect 0 3) (end-segment (make-segment (make-vect 0 0) (make-vect 0 3)))) "should select the start vector of the segment")
 )
 
 (run-registered-tests)
