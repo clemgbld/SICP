0e92b8a60fdf49eecb0ee2b03ad2d58888fe5047
cgombauld <clement.gombauld@cpexterne.org>
2024-01-20 10:36:26 +0100

    refacto ex 59 of chapter 2

--- ch-2/ex-2-59.scm
+++ ch-2/ex-2-59.scm
@@ -4,8 +4,7 @@
 (else (element-of-set? x (cdr set)))))
 
 (define (union-set set1 set2) 
-    (cond 
-        ((null? set2) set1) 
-        ((element-of-set? (car set2) set1) (union-set set1 (cdr set2)))
-        (else (cons (car set2) (union-set set1 (cdr set2))))
-))
+    (fold-right 
+        (lambda (curr acc) 
+            (if (element-of-set? curr set1) acc 
+                (cons curr acc))) set1 set2))
