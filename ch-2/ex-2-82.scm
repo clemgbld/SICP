8c4e4a1bcf2df17dfea98bd5e8327d3436fa63c7
cgombauld <clement.gombauld@cpexterne.org>
2024-02-06 20:49:05 +0100

    solve ex 82 of chapter 2

--- ch-2/ex-2-82.scm
+++ ch-2/ex-2-82.scm
@@ -4,22 +4,27 @@
         (if proc
             (apply proc (map contents args)) 
                 (if (> (length args) 1)
-        (let ((type1 (car type-tags)) 
-            (type2 (cadr type-tags)) 
-                (a1 (car args))
-                (a2 (cadr args)))
-        (let ((t1->t2 (get-coercion type1 type2)) 
-        (t2->t1 (get-coercion type2 type1)))
-            (cond (t1->t2
-                (apply-generic op (t1->t2 a1) a2))
-                        (t2->t1
-                         (apply-generic op a1 (t2->t1 a2)))
-            (else (error "No method for these types" 
-            (list op type-tags))))))
+        (coearce-all type-tags op args) 
               (error "No method for these types"
                      (list op type-tags)))))))
 
-(define (coearce-all type-tags op args coearced-args) 
-    (if (null? type-tags) (error "No method for these types"
-                     (list op type-tags)) 
-                     ))
+(define (coearce-all type-tags op args ) 
+     (define (recur  type-tags coearced-args arguments)
+      (cond ((null? type-tags) (error "No method for these types" (list op type-tags)) )
+             ((null? arguments) (apply-generic op coearced-arguments))
+             ((not ((get-coercion (car type-tags) (type-tag (car arguments))) (car arguments)) ) (coearce-all (cdr type-tags) '() args))
+             ((eq? (car type-tags) (type-tag (car arguments))) (coerce-all type-tags (append coearced-args (car arguments) ) (cdr arguments)))
+             (else (coearce-all type-tags 
+             (append coearced-args (((get-coercion (car type-tags) (type-tag (car arguments))) (car arguments)))) (cdr arguments)))))
+
+      (if (every? (lambda (a) (eq? (car type-tags) (type-tag a))) args) (error "No method for these types" (list op type-tags)) 
+      (recur type-tags '() args)) 
+    )
+
+(define (every? pred lst)
+  (cond ((null? lst) #t)                 
+        ((pred (car lst))                
+         (every? pred (cdr lst)))       
+        (else #f)))
+
+; this algorithm and the last one don't take into account that a coercion works but not procedure is found although another coercion could have work either
