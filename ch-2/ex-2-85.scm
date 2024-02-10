90e8d36fefe65104080c9c46d45adfd039a1988a
cgombauld <clement.gombauld@cpexterne.org>
2024-02-10 22:22:39 +0100

    solve ex 85 of chapter 2

--- ch-2/ex-2-85.scm
+++ ch-2/ex-2-85.scm
@@ -41,3 +41,21 @@
 
 (define (install-package-complex)
     (put 'project '(complex) project-complex)'done)
+
+(define (apply-generic op . args)
+    (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags))) 
+    (if proc
+(drop (apply proc (map contents args))) 
+(if (= (length args) 2)
+(let ((type1 (car type-tags)) 
+(type2 (cadr type-tags)) (a1 (car args))
+(a2 (cadr args)))
+(let 
+((higher-type (find-higher-type type1 type2)))
+(cond ((eq? type1 type2) (error "No method for these types" (list op type-tags))) 
+    ((eq? higher-type type1) (apply-generic op a1 (raise-to type1 a2)))
+    ((eq? higher-type type2) (apply-generic op (raise-to type2 a1) a2))
+(else (error "No method for these types" (list op type-tags))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))
