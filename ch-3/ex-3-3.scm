deb006322ed7d9ef4d1d0c5745754ad9f57c3ac3
cgombauld <clement.gombauld@cpexterne.org>
2024-02-17 13:29:10 +0100

    refacto

--- ch-3/ex-3-3.scm
+++ ch-3/ex-3-3.scm
@@ -7,14 +7,11 @@
     (if ( > amount  initial-amount) "Insufficient funds"
     (begin (set! initial-amount (- initial-amount amount)) initial-amount)))
 
-    (define (check-password entered-password proc) 
-        (lambda (amount) 
-        (if (eq? entered-password password) 
-            (proc amount) 
-            "Incorrect password")))
+    (define (reject-operation amount) "Incorrect password") 
 
 (lambda (entered-password m) 
-    (cond 
-        ((eq? m 'deposit) (check-password entered-password deposit))
-        ((eq? m 'withdraw) (check-password entered-password withdraw))
+    (cond
+        ((not (eq? entered-password password)) reject-operation) 
+        ((eq? m 'deposit) deposit)
+        ((eq? m 'withdraw) withdraw)
         (else(error "Unknown request: MAKE-ACCOUNT" m)))))
