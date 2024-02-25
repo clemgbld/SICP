7b6f46e28c98414565636cb5055fab03e8f43c07
cgombauld <clement.gombauld@cpexterne.org>
2024-02-25 18:22:17 +0100

    refacto with memq

--- ch-3/ex-3-17.scm
+++ ch-3/ex-3-17.scm
@@ -1,14 +1,7 @@
 (define (count-pairs x) 
-
-    (define (has-already-been-counted? l target) 
-        (if (null? l) #f 
-            (if (eq? target (car l)) #t 
-                (has-already-been-counted? (cdr l) target))))
-
     (define already-counted '())
-
     (define (recur x) 
-                (cond ((or (not (pair? x)) (has-already-been-counted? already-counted x))
+                (cond ((or (not (pair? x)) (memq x already-counted ))
                 0)
                 (else
                     (set! already-counted (cons x already-counted))
