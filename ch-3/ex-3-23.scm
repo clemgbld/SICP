e85b0285250efd3d7b1ec24e5f088a52432108f6
cgombauld <clement.gombauld@cpexterne.org>
2024-03-02 17:45:53 +0100

    solve ex 23 of chapter 3

--- ch-3/ex-3-23.scm
+++ ch-3/ex-3-23.scm
@@ -2,11 +2,14 @@
 
 (define (rear-ptr dequeue) (cdr dequeue))
 
-(define (set-front-ptr! dequeue item)
-    (set-car! dequeue item))
+(define (set-front-ptr! dq item)
+    (set-car!  dq item))
 
-(define (set-rear-ptr! dequeue item)
-    (set-cdr! dequeue item))
+(define (set-rear-ptr! dq item)
+    (set-cdr!   dq item))
+
+(define (set-rear-rear-ptr! dq item) 
+    (set-cdr! dq item))
 
 (define (empty-dequeue? dequeue)
     (null? (front-ptr dequeue)))
@@ -14,34 +17,73 @@
 (define (make-dequeue)
     (cons '() '()))
 
-(define (front-dequeue dequeue) 
-    (if (empty-dequeue? dequeue)
-      (error "FRONT called with an empty dequeue" dequeue)
-      (car (front-ptr dequeue))))
+(define (front-dequeue dq) 
+    (if (empty-dequeue? dq)
+      (error "FRONT called with an empty dequeue" dq)
+      (curr (front-ptr dq))))
 
-(define (rear-dequeue dequeue) 
-    (if (empty-dequeue? dequeue)
-      (error "REAR called with an empty dequeue" dequeue)
-      (car (rear-ptr dequeue))))
+(define (rear-dequeue dq) 
+    (if (empty-dequeue? dq)
+      (error "REAR called with an empty dequeue" dq)
+      (curr (rear-ptr dq))))
 
-(define (front-insert-dequeue! dequeue item)
-    (cond 
-        ((empty-dequeue? dequeue)
-            (let ((new-pair (cons item '())))
-                (set-front-ptr! dequeue new-pair)
-                (set-rear-ptr! dequeue new-pair)
-                dequeue))
-        (else 
-            (set-front-ptr! dequeue (cons item (front-ptr dequeue)))
-            dequeue)))
+(define (dequeue-to-list dq) 
+(if (empty-dequeue? dq) 
+    '() 
+    (cons (front-dequeue dq) 
+        (dequeue-to-list 
+        (cons (next (front-ptr dq)) (rear-ptr dq))))))
 
-(define (rear-insert-dequeue! dequeue item)
-    (let ((new-pair (cons item '())))
-        (cond ((empty-dequeue? dequeue) (set-front-ptr! dequeue new-pair)
-                (set-rear-ptr! dequeue new-pair)
-                dequeue)
-                (else 
-                    (set-cdr! (rear-ptr dequeue) new-pair)
-                    (set-rear-ptr! dequeue new-pair)
-                    dequeue))))
+(define (make-node prev curr next) 
+    (cons (cons prev curr) next))
 
+(define (curr node) 
+    (cdar  node))
+
+(define (next node)
+    (cdr node))
+
+(define (prev node)
+    (caar node))
+
+(define (set-prev! node prev-node) 
+    (set-car! (car node) prev-node))
+
+(define (set-next! node next-node)
+    (set-cdr! node next-node))
+
+(define (front-insert-dequeue! dq item) 
+        (cond ((empty-dequeue? dq) 
+            (let ((first-node (make-node '() item '())))
+                (set-front-ptr! dq  first-node )
+                (set-rear-ptr! dq first-node)
+                dq))
+            (else (let ((first-node (make-node '() item  (front-ptr dq))))
+                (set-prev! (front-ptr dq) first-node)
+                    (set-front-ptr! dq first-node)
+                    dq))))
+
+(define (rear-insert-dequeue! dq item)
+    (cond ((empty-dequeue? dq) 
+            (let ((first-node (make-node '() item '())))
+                (set-front-ptr! dq  first-node )
+                (set-rear-ptr! dq first-node)
+                dq))
+            (else (let ((last-node (make-node (rear-ptr dq) item '())))
+                (set-next! (rear-ptr dq) last-node)
+                (set-rear-ptr! dq last-node)))))
+
+(define (front-delete-dequeue! dq) 
+    (cond ((empty-dequeue? dq)
+      (error "FRONT DELETE called with an empty dq" dq))
+      (else 
+        (set-front-ptr! dq (next (front-ptr dq)))
+        (set-prev! (front-ptr dq) '()))))
+
+(define (rear-delete-dequeue! dq) 
+    (cond ((empty-dequeue? dq)
+      (error "REAR DELETE called with an empty dq" dq))
+      (else 
+            (set-next! (prev (rear-ptr dq)) '())
+            (set-rear-ptr! dq (prev (rear-ptr dq)))
+            dq)))
--- ch-3/tests/ex-3-23.test.scm
+++ ch-3/tests/ex-3-23.test.scm
@@ -1,42 +1,55 @@
 (load "test-manager/load.scm")
+(load "functions/deep-equal.scm")
 (load "ch-3/ex-3-23.scm")
 
+
 (define-each-test
+(check (equal? (cons '() '()) (make-dequeue)) "should be an empty dequeue")
+
 (let ((dq (make-dequeue)))
     (front-insert-dequeue! dq 'a)
-    (check (equal? (cons '(a) (cons 'a '())) dq) "should be able to add an item in the front of the dequeue when the dequeue is empty"))
+    (check (equal? '(a) (dequeue-to-list dq) ) "should be able to add an item in front of an empty dequeue"))
 
 (let ((dq (make-dequeue)))
     (front-insert-dequeue! dq 'a)
     (front-insert-dequeue! dq 'b)
-    (check (equal? (cons '(b a) (cons 'a '())) dq) "should add an item to the front of the dequeue when there is already some item in the dequeue"))
+    (check (equal? '(b a) (dequeue-to-list dq) ) "should be able to add an item in front of a non empty dequeue"))
 
 (let ((dq (make-dequeue)))
     (rear-insert-dequeue! dq 'a)
-    (check (equal? (cons '(a) (cons 'a '())) dq) "should be able to an item at the end of the dequeue when the dequeue is empty"))
-
-(let ((dq (make-dequeue)))
-    (rear-insert-dequeue! dq 'a)
-    (rear-insert-dequeue! dq 'b)
-    (check (equal? (cons '(a b) (cons 'b '())) dq) "should be able to an item at the end of the dequeue when the dequeue is not empty"))
-
-(let ((dq (make-dequeue)))
-    (front-insert-dequeue! dq 'a)
-    (check (equal? 'a (front-dequeue dq)) "should be able to get the first element of the dequeue"))
+    (check (equal? '(a) (dequeue-to-list dq) ) "should be able to add an item at the end of an empty dequeue"))
 
 (let ((dq (make-dequeue)))
     (rear-insert-dequeue! dq 'a)
     (rear-insert-dequeue! dq 'b)
-    (check (equal? 'b (rear-dequeue dq)) "should be able to get the rear of a dequeue"))
+    (check (equal? '(a b) (dequeue-to-list dq) ) "shoud be able to add an item at the end of a non empty"))
 
 (let ((dq (make-dequeue)))
-
     (rear-insert-dequeue! dq 'a)
     (rear-insert-dequeue! dq 'b)
-    (rear-insert-dequeue! dq 'c)
     (front-delete-dequeue! dq)
+    (check (equal? '(b) (dequeue-to-list dq) ) "should be able to delete at the front of the dequeue"))
 
-    (check (equal? (cons '(b c) (cons 'c '()))) "should be able to delete the first element of a dequeue"))
+(let ((dq (make-dequeue)))
+    (rear-insert-dequeue! dq 'a)
+    (rear-insert-dequeue! dq 'b)
+    (rear-delete-dequeue! dq)
+    (check (equal? '(a) (dequeue-to-list dq) ) "should be able to delete at the end of the dequeue"))
+
+(let ((dq (make-dequeue)))
+    (rear-insert-dequeue! dq 'a)
+    (rear-insert-dequeue! dq 'b)
+    (check (equal? 'b (rear-dequeue dq) ) "should retrieve the last item of a dequeue"))
+
+(let ((dq (make-dequeue)))
+    (rear-insert-dequeue! dq 'a)
+    (rear-insert-dequeue! dq 'b)
+    (rear-delete-dequeue! dq)
+    (front-insert-dequeue! dq 'c)
+    (front-insert-dequeue! dq 'e)
+    (front-delete-dequeue! dq)
+    (check (equal? '(c a) (dequeue-to-list dq) ) "should pass the acceptance test"))
+
 )
 
 
