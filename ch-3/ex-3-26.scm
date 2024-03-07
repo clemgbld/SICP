7ac7fe0d57cca93ed87c4761234adf4719e79a0c
cgombauld <clement.gombauld@cpexterne.org>
2024-03-07 08:53:43 +0100

    save

--- ch-3/ex-3-26.scm
+++ ch-3/ex-3-26.scm
@@ -9,26 +9,38 @@
 
     (define (right-branch tree) (caddr tree))
 
+    (define (left-branch tree) (cadr tree))
+
+    (define (set-left-branch! tree value)
+        (set-car! (cdr tree) value))
+
     (define (set-right-branch! tree value)
         (set-cdr! (cdr tree) (cons value '())))
 
 
         (define (lookup keys) 
-
             (define (recur keys tree) 
                 (if (null? tree) 
                     #f
-                    (if (equal? (car keys) (car (entry tree)))
-                        (cdr (entry tree))
-                        (recur keys (right-branch tree)))))
+                    (let ((key (car keys)) 
+                        (head-key (car (entry tree))))
+                        (cond ((equal? key head-key)
+                        (cdr (entry tree)))
+                       ((key<? key head-key)
+                            (recur keys (left-branch tree))) 
+                        (else (recur keys (right-branch tree)))))))
 
             (recur keys root-node))
 
     (define (insert! keys value) 
+    
         (define (recur keys tree)
-            (cond ((key<? (car keys) (car (entry tree)))
-                'not-implemented)
-                (else (set-right-branch! tree (make-tree (cons (car keys) value) '() '())))))
+            (let ((new-node (make-tree (cons (car keys) value) '() '())))
+                (cond ((key<? (car keys) (car (entry tree)))
+                (set-left-branch! tree new-node))
+                (else (if (null? (right-branch tree)) 
+                    (set-right-branch! tree new-node)
+                        (recur keys (right-branch tree)))))))
 
         (if (null? root-node) 
             (set! root-node (make-tree (cons (car keys ) value) '() '()))
--- ch-3/tests/ex-3-26.test.scm
+++ ch-3/tests/ex-3-26.test.scm
@@ -18,6 +18,15 @@
    ((table 'insert-proc!) (list 2) 'b) 
    ((table 'insert-proc!) (list -1) 'c) 
    (check (equal? 'b ((table 'lookup-proc)(list 2))) "should be able to find the second element stored in the lookup table")
+   (check (equal? 'c ((table 'lookup-proc)(list -1))) "should be able to find the third element stored in the lookup table")
+   )
+
+(let ((table (make-table < )))
+   ((table 'insert-proc!) (list 1) 'a) 
+   ((table 'insert-proc!) (list 2) 'b) 
+   ((table 'insert-proc!) (list -1) 'c) 
+   ((table 'insert-proc!) (list 4) 'd) 
+   (check (equal? 'b ((table 'lookup-proc)(list 2))) "should find a stored element in a deep tree")
    )
 
 )
