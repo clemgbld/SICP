dab2b67b094287381fd145a47c50b3470acb77e0
cgombauld <clement.gombauld@cpexterne.org>
2024-03-10 20:20:00 +0100

    save

--- ch-3/ex-3-28.scm
+++ ch-3/ex-3-28.scm
@@ -3,7 +3,7 @@
         (let ((new-value 
             (logical-or (get-signal a1) (get-signal a2))))
                 (after-delay
-                and-gate-delay
+                or-gate-delay
                 (lambda () (set-signal! output new-value)))))
     (add-action! a1 or-action-procedure)
     (add-action! a2 or-action-procedure)
