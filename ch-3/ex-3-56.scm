e24b19fb8f975c874eae527eac2d79d2cb6da5d7
cgombauld <clement.gombauld@cpexterne.org>
2024-03-29 19:31:31 +0100

    refacto

--- ch-3/ex-3-56.scm
+++ ch-3/ex-3-56.scm
@@ -1,4 +1,9 @@
 (load "functions/streams.scm")
 
 (define S 
-    (cons-stream 1 (merge (merge (scale-stream S 2) (scale-stream S 3)) (scale-stream S 5))))
+    (cons-stream 1 
+                (merge 
+                    (merge 
+                    (scale-stream S 2) 
+                    (scale-stream S 3)) 
+                (scale-stream S 5))))
