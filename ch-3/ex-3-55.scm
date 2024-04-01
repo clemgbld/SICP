55606b21f7a60d3935b111530a5d4b79aefbdb09
cgombauld <clement.gombauld@cpexterne.org>
2024-04-01 11:31:44 +0200

    solve ex 60 of chapter 3

--- ch-3/ex-3-55.scm
+++ ch-3/ex-3-55.scm
@@ -1,5 +1,7 @@
 (load "functions/streams.scm")
 
 (define (partial-sums s)
-    (define ps (cons-stream (stream-car s) (add-streams (partial-sums s) (stream-cdr s))))
+    (define ps (cons-stream 
+        (stream-car s) 
+        (add-streams (partial-sums s) (stream-cdr s))))
     ps)
