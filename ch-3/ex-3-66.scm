dd80be52776ed8c0beabdd5a45a4478973bf58c7
cgombauld <clement.gombauld@cpexterne.org>
2024-04-05 09:34:58 +0200

    solve ex 66 of chapter 3

--- ch-3/ex-3-66.scm
+++ ch-3/ex-3-66.scm
@@ -2,3 +2,9 @@
 
 (define integer-pairs (pairs integers integers))
 
+;the sream interleave pairs and double it every time the a left part of a pair is increased for each left part
+
+; f(n,m) m>=n (m,n is Z+) - 1
+; (m-n=0):(2^n - 1) - 1 
+; (m-n=1):((2^n - 1) + 2^(n - 1)) - 1 
+; (m-n>1):((2^n - 1) + 2^(n - 1) + (m - n - 1) * 2^n) - 1 
