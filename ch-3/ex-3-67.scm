bea080963427f3c4a065ad899f69b38dfa3e03aa
cgombauld <clement.gombauld@cpexterne.org>
2024-04-06 12:43:21 +0200

    solve ex 67 of chapter 3

---
+++ ch-3/ex-3-67.scm
@@ -0,0 +1,15 @@
+
+(load "functions/streams.scm")
+
+(define (make-pair head)
+    (lambda (tail) (list head tail)))
+
+(define (all-pairs s t) 
+    (define (recur s1 s2 indexes-stream)
+        (stream-append (stream-map (make-pair (stream-car s1)) (stream-take t (stream-car indexes-stream)))
+            (cons-stream
+                (list (stream-car s1) (stream-car s2))
+                    (interleave
+                        (stream-map (make-pair (stream-car s1)) (stream-cdr s2))
+                        (recur (stream-cdr s1) (stream-cdr s2) (stream-cdr indexes-stream))))))
+    (recur s t (integers-starting-from 0)))
