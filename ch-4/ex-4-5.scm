5407ad1f50e7dc5e6c0222bfe662021381d1ac86
cgombauld <clement.gombauld@cpexterne.org>
2024-04-24 08:55:33 +0200

    solve ex 5 of chapter 4

--- ch-4/ex-4-5.scm
+++ ch-4/ex-4-5.scm
@@ -17,6 +17,9 @@
 
 (define (recipient clause) (caddr clause))
 
+(define (make-lambda parameters body) 
+  (cons 'lambda (cons parameters body)))
+
 
 (define (make-if predicate consequent alternative)
         (list 'if predicate consequent alternative))
@@ -30,16 +33,30 @@
 
 (define (make-begin seq) (cons 'begin seq))
 
+ (define make-application 
+   (lambda (operator operands) 
+     (cons operator operands))) 
+  
+ (define make-let 
+   (lambda (var val body) 
+     (make-application (make-lambda (list var) body) val))) 
+
 (define (expand-clauses clauses)
-(if (null? clauses)
-'false
-(let ((first (car clauses)) (rest (cdr clauses)))
-(if (cond-else-clause? first) (if (null? rest)
+        (if (null? clauses)
+        'false
+                (let ((first (car clauses)) (rest (cdr clauses)))
+                        (if (cond-else-clause? first)
+                         (if (null? rest)
                 (sequence->exp (cond-actions first))
                 (error "ELSE clause isn't last: COND->IF"
                        clauses))
-            (make-if (cond-predicate first)
-                     (if (recipient? first)
-                     (list  (recipient first) (cond-predicate first)) 
-                     (sequence->exp (cond-actions first)))
-                     (expand-clauses rest))))))
+            (if (recipient? first)
+                    (make-let 'result 
+                        (cond-predicate first) 
+                        (make-if 'result 
+                                 (make-application (recipient first) 
+                                                   (list 'result)) 
+                                 (expand-clauses rest))) 
+             (make-if (cond-predicate first)
+                     (sequence->exp (cond-actions first))
+                     (expand-clauses rest)))))))
--- ch-4/tests/ex-4-5.test.scm
+++ ch-4/tests/ex-4-5.test.scm
@@ -6,7 +6,7 @@
 (check (equal? (list 'if '(= x 1) '(+ x 3)  'false) (cond->if (list 'cond '((= x 1) (+ x 3))))) "should be able to transform one cond clause in a if")
 (check (equal? (list 'if '(= x 1) '(+ x 3)  '(- x 1)) (cond->if (list 'cond '((= x 1) (+ x 3)) '(else (- x 1))))) "should be able to transform a cond to an if with the else as the alternative")
 (check (equal? (list 'if '(= x 1) '(+ x 3) (list 'if '(= x 2) '(- x 1) 'false)) (cond->if (list 'cond '((= x 1) (+ x 3)) '((= x 2) (- x 1))))) "should be able to transform cond into nested if")
-(check (equal? (list 'if '(assoc b ((a 1) (b 2))) '(cadr (assoc b ((a 1) (b 2)))) #f) (cond->if (list 'cond '((assoc b ((a 1) (b 2))) => cadr) '(else #f)))) "should be able to transform cond into if with the recipient syntax")
+(check (equal? (list '(lambda (result) if result (cadr result) #f) 'assoc 'b '((a 1) (b 2))) (cond->if (list 'cond '((assoc b ((a 1) (b 2))) => cadr) '(else #f)))) "should be able to transform cond into if with the recipient syntax")
 )
 
 
