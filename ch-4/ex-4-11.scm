168959ba9cfa81fb82c62c7fbd8a695ebc13a6a9
cgombauld <clement.gombauld@cpexterne.org>
2024-05-01 16:49:49 +0200

    save

--- ch-4/ex-4-11.scm
+++ ch-4/ex-4-11.scm
@@ -4,3 +4,24 @@
         (cons 
             (cons (car variables) (car values)) 
             (make-frame (cdr variables) (cdr values)))))
+
+
+
+(define (add-binding-to-frame! var val frame)
+    (let ((first-var (caar frame)) (first-val (cdar frame)))
+        (set-car! frame (cons var val) )
+        (set-cdr! frame (cons (cons first-var first-val) (cdr frame)))))
+
+(define (extend-environment vars vals base-env) 
+(if (= (length vars) (length vals))
+    (cons (make-frame vars vals) base-env) 
+    (if (< (length vars) (length vals))
+          (error "Too many arguments supplied" vars vals)
+          (error "Too few arguments supplied" vars vals))))
+
+(define (lookup-variable-value var env)
+    (define (scan frame)
+        (cond ((eq? (caar frame) var) (cdar frame))
+        (else (scan (cdr frame)))))
+    (let ((frame (first-frame env)))
+        (scan frame)))
--- ch-4/tests/ex-4-11.test.scm
+++ ch-4/tests/ex-4-11.test.scm
@@ -9,6 +9,22 @@
 (check (equal? (list (cons 'x 1) (cons 'y 2) (cons 'z 3)) (make-frame '(x y z) '(1 2 3))) "should be a frame of multiple elements")
 )
 
+(define-each-test
+(let ((frame (make-frame '(x y z) '(1 2 3))))
+    (add-binding-to-frame! 'a 4 frame)
+    (check (equal? (list (cons 'a 4) (cons 'x 1) (cons 'y 2) (cons 'z 3)) frame )) "should be able to add a binding to a frame")
+)
+
+(define-each-test
+(check (equal? (cons '((a . 1) (b . 2)) (cons '((x . 3)) '())) (extend-environment '(a b) '(1 2) (cons (make-frame '(x) '(3)) '()))) "should extend the environement")
+)
+
+(define-each-test
+(check (= 1 (lookup-variable-value 'a (cons '((a . 1)) '()))) "should find the expected value")
+(check (= 2 (lookup-variable-value 'a (cons '((a . 2)) '()))) "should find the expected value")
+(check (= 2 (lookup-variable-value 'a (cons '((b . 1) (a . 2)) '()))) "should find the expected value")
+)
+
 
 
 
