457c45a9e98b91339f7cd57c6dfad5ce6bf2af41
cgombauld <clement.gombauld@cpexterne.org>
2024-05-14 15:21:48 +0200

    refacto

--- ch-4/ex-4-27.scm
+++ ch-4/ex-4-27.scm
@@ -8,8 +8,8 @@
 ;;; L-Eval input:
 ;w
 ;;; L-Eval value:
-;10 id has been applied twice with 10 so the first time it returned 11 and the second time 12
+;10 
 ;;; L-Eval input: 
 ;count
 ;;; L-Eval value: 
-;2 count is now 12 because w was needed to print its value
+;2 count is now 2 because w was needed to print its value
