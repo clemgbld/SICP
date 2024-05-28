d462eec8730f5a0bd2edd347030e9f8b6556ff5e
cgombauld <clement.gombauld@cpexterne.org>
2024-05-28 09:39:01 +0200

    refacto

--- ch-4/ex-4-38.scm
+++ ch-4/ex-4-38.scm
@@ -25,7 +25,7 @@
           (list 'smith smith))))
 
 ; ((baker 1)(cooper 2) (fletcher 4) (miller 3) (smith 5))
-; ((baker 3)(cooper 2) (fletcher 4) (miller 5) (smith 1))
 ; ((baker 1)(cooper 2) (fletcher 4) (miller 5) (smith 3))
 ; ((baker 1)(cooper 4) (fletcher 2) (miller 5) (smith 3))
+; ((baker 3)(cooper 2) (fletcher 4) (miller 5) (smith 1))
 ; ((baker 3)(cooper 4) (fletcher 2) (miller 5) (smith 1))
