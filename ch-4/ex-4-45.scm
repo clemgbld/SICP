bd9befd2152fe445dacd1a317a28762bfd13bdfe
cgombauld <clement.gombauld@cpexterne.org>
2024-06-04 10:02:54 +0200

    started to solve ex 45 of chapter 4

---
+++ ch-4/ex-4-45.scm
@@ -0,0 +1,25 @@
+(parse '(the professor lectures to the student in the class with the cat))
+
+;1
+(sentence 
+    (simple-noun-phrase (article the) (noun professor))
+    (verb-phrase 
+        (verb-phrase 
+            (verb lectures)
+            (prep-phrase 
+                (prep to)
+                (noun-phrase 
+                    (simple-noun-phrase 
+                        (article the) 
+                        (noun student))
+                    (prep-phrase 
+                        (prep in)
+                        (noun-phrase 
+                            (simple-noun-phrase 
+                                (article the)
+                                (noun class))
+                            (prep-phrase 
+                                (prep with)
+                                (simple-noun-phrase 
+                                    (article the)
+                                    (noun cat))))))))))
