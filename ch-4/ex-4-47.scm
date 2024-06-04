e47c83a5ed9ed13dcdb2172fac8adf4be209b30d
cgombauld <clement.gombauld@cpexterne.org>
2024-06-04 20:28:07 +0200

    solve ex 47 of chapter 4

---
+++ ch-4/ex-4-47.scm
@@ -0,0 +1,21 @@
+;Orginal version
+(define (parse-verb-phrase)
+(define (maybe-extend verb-phrase)
+    (amb verb-phrase
+    (maybe-extend
+          (list 'verb-phrase
+                verb-phrase
+                (parse-prepositional-phrase)))))
+  (maybe-extend (parse-word verbs)))
+
+;Louis Reasoner's version
+(define (parse-verb-phrase) 
+    (amb (parse-word verbs) 
+        (list 'verb-phrase
+                (parse-verb-phrase)
+                (parse-prepositional-phrase))))
+
+;Louis Reasoner's version has a subtle bug in it that cause 
+;an infinite loop to occur because if the (parse-prepositonal-phase) doesn't succeed and the (parse-verb-phrase) before it succed 
+;the program try to call (parse-verb-phrase) indefinetly
+
