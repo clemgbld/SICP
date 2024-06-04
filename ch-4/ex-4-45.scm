f017e5b929a92afcf9a03be49e4d520fe16e6f3b
cgombauld <clement.gombauld@cpexterne.org>
2024-06-04 19:52:34 +0200

    solve the exercise 45 of chapter 4

--- ch-4/ex-4-45.scm
+++ ch-4/ex-4-45.scm
@@ -1,25 +1,119 @@
 (parse '(the professor lectures to the student in the class with the cat))
 
 ;1
-(sentence 
-    (simple-noun-phrase (article the) (noun professor))
+(sentence (simple-noun-phrase 
+    (article the) 
+    (noun professor))
     (verb-phrase 
         (verb-phrase 
             (verb lectures)
             (prep-phrase 
                 (prep to)
                 (noun-phrase 
-                    (simple-noun-phrase 
-                        (article the) 
+                    (simple-noun-phrase
+                        (article the)
                         (noun student))
-                    (prep-phrase 
-                        (prep in)
-                        (noun-phrase 
-                            (simple-noun-phrase 
-                                (article the)
-                                (noun class))
                             (prep-phrase 
-                                (prep with)
+                                (prep in)
                                 (simple-noun-phrase 
                                     (article the)
-                                    (noun cat))))))))))
+                                    (noun class))))))
+    (prep-phrase 
+        (prep with) 
+        (simple-noun-phrase 
+            (article the)
+            (noun cat)))))
+
+;2
+(sentence (simple-noun-phrase 
+    (article the) 
+    (noun professor))
+    (verb-phrase 
+        (verb-phrase 
+            (verb lectures)
+            (prep-phrase 
+                (prep to)
+                    (simple-noun-phrase
+                        (article the)
+                        (noun student))))
+    (prep-phrase 
+        (prep in) 
+        (noun-phrase 
+            (simple-noun-phrase 
+                (article the)
+                (noun class))
+            (prep-phrase (prep with)
+                (simple-noun-phrase 
+                    (article the) 
+                    (noun cat)))))))
+
+;3
+(sentence (simple-noun-phrase 
+    (article the) 
+    (noun professor))
+    (verb-phrase 
+        (verb-phrase 
+           (verb-phrase 
+                (verb lectures)
+                (prep-phrase 
+                    (prep to)
+                    (simple-noun-phrase 
+                        (article the) 
+                        (noun student)))) 
+            (prep-phrase 
+                        (prep in)
+                            (simple-noun-phrase 
+                                (article the)
+                                (noun class))))
+    (prep-phrase 
+        (prep with) 
+        (simple-noun-phrase 
+            (article the)
+            (noun cat)))))
+
+;4 
+(sentence (simple-noun-phrase 
+    (article the) 
+    (noun professor))
+    (verb-phrase 
+        (verb-phrase 
+            (verb lectures)
+            (prep-phrase 
+                (prep to)
+                    (noun-phrase 
+                        (simple-noun-phrase
+                            (article the)
+                            (noun student))
+                        (prep-phrase 
+                            (prep in)
+                            (simple-noun-phrase
+                                (article the)
+                                (noun class))))))
+    
+            (prep-phrase (prep with)
+                (simple-noun-phrase 
+                    (article the) 
+                    (noun cat)))))
+
+;5
+(sentence (simple-noun-phrase 
+    (article the) 
+    (noun professor))
+    (verb-phrase 
+       (verb lectures) 
+    (prep-phrase 
+        (prep to) 
+        (noun-phrase 
+            (simple-noun-phrase 
+                (article the)
+                (noun student))
+            (prep-phrase (prep in)
+                (noun-phrase
+                    (simple-noun-phrase
+                        (article the)
+                        (noun class))
+                    (prep-phrase
+                            (prep with)
+                            (simple-noun-phrase 
+                                (article the)
+                                (noun cat)))))))))
