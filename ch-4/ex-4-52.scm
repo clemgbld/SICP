166f45e197ddd82aebe18b0c6c930df02452db92
cgombauld <clement.gombauld@cpexterne.org>
2024-06-13 08:42:25 +0200

    sovle ex 53 of chapter 4

--- ch-4/ex-4-52.scm
+++ ch-4/ex-4-52.scm
@@ -373,6 +373,7 @@
         (list 'remainder remainder)
         (list 'integer? integer?)
         (list 'sqrt sqrt)
+        (list 'square square)
         (list 'even? even?)
         (list 'eq? eq?)
 ;;      more primitives
@@ -405,11 +406,42 @@
         '()
             (cons low (enumerate-interval (+ low 1) high))))
 
-(define (require p) (if (not p) (ramb)))
+(define (require p) (if (not p) (amb)))
 
 (define (an-element-of items)
 (require (not (null? items)))
 (amb (car items) (an-element-of (cdr items))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n)
+         n)
+        ((divides? test-divisor n)
+         test-divisor)
+        (else (find-divisor
+               n
+               (+ test-divisor 1)))))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (prime-sum-pair list1 list2) 
+  (let ((a (an-element-of list1))
+        (b (an-element-of list2)))
+    (require (prime? (+ a b)))
+    (list a b)))
+
+(let ((pairs '())) (if-fail
+(let ((p (prime-sum-pair '(1 3 5 8)
+'(20 35 110))))
+     (permanent-set! pairs (cons p pairs))
+     (amb))
+   pairs))
 
 (if-fail 
     (let ((x (an-element-of '(1 3 5)))) 
