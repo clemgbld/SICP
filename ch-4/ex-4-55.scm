bc052e685a378f95b183e7f0415a1cc4e5659c04
cgombauld <clement.gombauld@cpexterne.org>
2024-06-15 14:12:16 +0200

    solve ex 55 of chapter 4

---
+++ ch-4/ex-4-55.scm
@@ -0,0 +1,11 @@
+;a) all people supervised by Ben Bitdiddle;
+
+(supervisor ?x (Bitdiddle Ben))
+
+;b) all the names and jobs of all people in the accounting division;
+
+(job ?x (accounting . ?y))
+
+;c) the names and addresses of all people who live in Slumerville
+
+(address ?x (Slumerville . ?y))
