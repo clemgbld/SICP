7b1ed9cc5255bf027745bd2905e68ae6e325384e
cgombauld <clement.gombauld@cpexterne.org>
2024-06-17 09:06:18 +0200

    fix ex 2 17 of chapter 2

--- ch-2/ex-2-17.scm
+++ ch-2/ex-2-17.scm
@@ -1,6 +1,9 @@
 (define (last-pair-in-list l)
     (define (last-pair-iter l last)
-        (if (null? l) last 
+        (if (null? l) 
+            (if last 
+                (list last) 
+                #f) 
             (last-pair-iter (cdr l) 
                     (if (even? (car l)) 
                             (car l) 
