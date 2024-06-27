4413c8758d0827920c41fa58df3914d86682b6d0
cgombauld <clement.gombauld@cpexterne.org>
2024-06-27 08:44:45 +0200

    solve ex 74 of chapter 4

---
+++ ch-4/ex-4-74.scm
@@ -0,0 +1,11 @@
+;a)
+
+(define (simple-stream-flatmap proc s)
+    (simple-flatten (stream-map proc s)))
+
+(define (simple-flatten stream)
+    (stream-map stream-car 
+        (stream-filter (lambda (s) (not (stream-null? s))) stream)))
+
+;b) not i won't 
+
