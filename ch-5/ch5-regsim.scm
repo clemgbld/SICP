ce15c54cd5ecc39c2371fe21ab8e679f69fc27db
cgombauld <clement.gombauld@cpexterne.org>
2024-07-07 12:46:37 +0200

    solve ex 8 of chapter 5

--- ch-5/ch5-regsim.scm
+++ ch-5/ch5-regsim.scm
@@ -173,17 +173,20 @@
       (update-insts! insts labels machine)
       insts)))
 
-(define (extract-labels text receive)
+(define (extract-labels text receive) 
   (if (null? text)
       (receive '() '())
-      (extract-labels (cdr text)
-       (lambda (insts labels)
-         (let ((next-inst (car text)))
-           (if (symbol? next-inst)
-               (receive insts
+      (extract-labels
+(cdr text)
+(lambda (insts labels)
+(let ((next-inst (car text))) 
+  (if (symbol? next-inst)
+        (if (assoc next-inst labels) 
+          (error "LABEL already defined" next-inst)
+          (receive insts
                         (cons (make-label-entry next-inst
-                                                insts)
-                              labels))
+                              insts)
+                              labels)))
                (receive (cons (make-instruction next-inst)
                               insts)
                         labels)))))))
---
+++ ch-5/ex-5-8.scm
@@ -0,0 +1,47 @@
+(load "ch-5/ch5-regsim.scm")
+
+(define bad-machine 
+  (make-machine 
+    '(a)
+    (list )
+    '(
+
+      start
+        (goto (label here))
+      here
+        (assign a (const 3))
+        (goto (label there))
+      here
+        (assign a (const 4))
+        (goto (label there))
+there
+       )
+    )
+  )
+
+; the content of the register a will be 3 because assoc take the first occurence in a the list
+;
+; 
+(define (extract-labels text receive) 
+  (if (null? text)
+      (receive '() '())
+      (extract-labels
+(cdr text)
+(lambda (insts labels)
+(let ((next-inst (car text))) 
+  (if (symbol? next-inst)
+        (if (assoc next-inst labels) 
+          (error "LABEL already defined" next-inst)
+          (receive insts
+                        (cons (make-label-entry next-inst
+                              insts)
+                              labels)))
+               (receive (cons (make-instruction next-inst)
+                              insts)
+                        labels)))))))
+
+;
+;
+
+  
+
