e113b6713cfa9c2c492c9ec5d521079acaf8056c
cgombauld <clement.gombauld@cpexterne.org>
2024-07-29 10:37:20 +0200

    solve first part of ex 21 of chpater 5

--- ch-5/ex-5-21.scm
+++ ch-5/ex-5-21.scm
@@ -1,6 +1,40 @@
 (load "ch-5/ch5-regsim.scm")
 
 (define make-count-leaves-machine-rec 
-(lambda () (make-machine '(tree count) (list ) '((assign count (const 0) )) ))  )
+(lambda () 
+  (make-machine '(tree count first continue) 
+                (list (list '+ +) 
+                      (list 'car car) 
+                      (list 'cdr cdr ) 
+                      (list 'null? null? )
+                      (list 'pair? pair? )
+                      (list 'not-pair?' (lambda (p) (not (pair? p)) ) )
+                      ) 
+                '(
+                  (assign count (const 0) )
+                  (assign continue (label done) )
+                  loop
+                  (test (op null?) (reg tree))
+                  (branch (label imediate-answer) )
+                  (assign first (op car) (reg tree) )
+                  (assign tree (op cdr) (reg tree))
+                  (test (op pair?) (reg first))
+                  (branch (label node) )
+                  (assign count (op +) (reg count) (const 1) )
+                  (goto (label loop))
+                  node
+                  (save continue)
+                  (save tree)
+                  (assign tree (reg first) )
+                  (assign continue (label done-node) )
+                  (goto (label loop) )
+                  imediate-answer
+                  (goto (reg continue))
+                  done-node
+                  (restore tree)
+                  (restore continue)
+                  (goto (label loop) )
+                  done 
+                  ))))
 
 
--- ch-5/tests/ex-5-21.test.scm
+++ ch-5/tests/ex-5-21.test.scm
@@ -7,6 +7,25 @@
     (start machine)
     (check (= (get-register-contents machine 'count ) 0) "should not count anything when there is no branch" )
     )
+
+(let ((machine (make-count-leaves-machine-rec)))
+    (set-register-contents! machine 'tree '(1))
+    (start machine)
+    (check (= (get-register-contents machine 'count ) 1) "should be able to count the branch of the simplest tree" )
+    )
+
+(let ((machine (make-count-leaves-machine-rec)))
+    (set-register-contents! machine 'tree '(1 2 3))
+    (start machine)
+    (check (= (get-register-contents machine 'count ) 3) "should be able to count the branches of a simple flat tree" )
+    )
+
+(let ((machine (make-count-leaves-machine-rec)))
+    (set-register-contents! machine 'tree '(1 (1 2) 3))
+    (start machine)
+    (check (= (get-register-contents machine 'count ) 4) "should be able to count the branches of a multi level tree" )
+    )
+
 )
 
 
