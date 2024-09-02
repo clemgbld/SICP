fa9048b24d776bfca91cacd318da77d358f58d0f
cgombauld <clement.gombauld@cpexterne.org>
2024-09-02 09:08:28 +0200

    solve ex 39 of chapter 5

--- ch-5/ex-5-39.scm
+++ ch-5/ex-5-39.scm
@@ -1,12 +1,17 @@
 (load "ch-5/ch5-eceval-support.scm")
 
+(define (make-lexical-address frame-address value-address )
+ (cons frame-address value-address))
+
 (define (get-value-address address) (cdr address))
 
 (define (get-scope-address address) (car address))
 
 (define (find-frame n env ) 
-  (if (= n 0) (first-frame env)
-   (find-frame (- n 1) (enclosing-environment env ))))
+  (cond 
+    ((null? env) (error "Unbound variable"))
+    ((= n 0) (first-frame env))
+    (else (find-frame (- n 1) (enclosing-environment env )))))
 
 (define (lexical-address-lookup address env) 
   (let ((value (list-ref 
@@ -15,3 +20,12 @@
       (if (eq? value '*unassigned*) 
         (error value " SHOULD NOT BE UNASSIGNED" ) 
         value)))
+
+(define (lexical-address-set! address env val)
+  (let* ((value-tail (frame-values 
+             (list-tail (find-frame (get-scope-address address) env) 
+                        (get-value-address address)))) 
+         (value (car value-tail)))
+     (if (eq? value  '*unassigned*) 
+       (error value " SHOULD NOT BE UNASSIGNED --SET" ) 
+       (set-car! value-tail val))))
--- ch-5/tests/ex-5-39.test.scm
+++ ch-5/tests/ex-5-39.test.scm
@@ -2,10 +2,41 @@
 (load "ch-5/ex-5-39.scm")
 
 (define-each-test
-  (check (= (lexical-address-lookup (cons 0 0) (cons (make-frame '(a) '(1) ) '())) 1) "should find the first variable in the first frame of the env") 
-  (check (= (lexical-address-lookup (cons 0 1) (cons (make-frame '(a b) '(1 2) ) '())) 2) "should the second variable in the first frame of the env") 
-  (check (= (lexical-address-lookup (cons 1 2) ( cons (make-frame '(a b ) '(1 2)) (cons (make-frame '(a b c) '(5 6 7) ) '())) ) 7) "should find the value in a nested env") 
-  (check (= (lexical-address-lookup (cons 2 1) ( cons (make-frame '(a b ) '(1 2)) (cons (make-frame '(a b c) '(5 6 7) ) (cons (make-frame '(c d) '(110 12) ) '() ) )) ) 12) "should find the value in a nested env") 
+  (check (= (lexical-address-lookup (make-lexical-address 0 0) (cons (make-frame '(a) '(1) ) '())) 1) "should find the first variable in the first frame of the env") 
+  (check (= (lexical-address-lookup (make-lexical-address 0 1) (cons (make-frame '(a b) '(1 2) ) '())) 2) "should the second variable in the first frame of the env") 
+  (check (= (lexical-address-lookup (make-lexical-address 1 2) ( cons (make-frame '(a b ) '(1 2)) (cons (make-frame '(a b c) '(5 6 7) ) '())) ) 7) "should find the value in a nested env") 
+  (check (= (lexical-address-lookup (make-lexical-address 2 1) 
+  ( cons (make-frame '(a b ) '(1 2)) 
+         (cons (make-frame '(a b c) '(5 6 7) ) 
+               (cons (make-frame '(c d) '(110 12) ) '() ) )) ) 12) "should find the value in a nested env") 
+    )
+
+(define-each-test
+  (let ((env (cons (make-frame '(a) '(1) ) '())))
+    (lexical-address-set! (make-lexical-address 0 0) env 2)
+    (check (equal? env  (cons (make-frame '(a) '(2) ) '())) "should set the first element of the first frame in the environment"))
+
+(let ((env (cons (make-frame '(a b c d) '(1 2 3 4) ) '())))
+    (lexical-address-set! (make-lexical-address 0 2) env 10)
+    (check (equal? env  (cons (make-frame '(a b c d) '(1 2 10 4) ) '())) "should set the an element of the first frame in the environment"))
+
+(let ((env (cons (make-frame '(a b c d) '(1 2 3 4) ) '())))
+    (lexical-address-set! (make-lexical-address 0 2) env 10)
+    (check (equal? env  (cons (make-frame '(a b c d) '(1 2 10 4) ) '())) "should set the an element of the first frame in the environment"))
+
+(let ((env ( cons (make-frame '(a b ) '(1 2)) (cons (make-frame '(a b c) '(5 6 7) ) '()))))
+    (lexical-address-set! (make-lexical-address 1 2) env 12)
+    (check (equal? env  ( cons (make-frame '(a b ) '(1 2)) (cons (make-frame '(a b c) '(5 6 12) ) '()))) "should set the an element of a nested frame"))
+
+
+(let ((env ( cons (make-frame '(a b ) '(1 2)) 
+         (cons (make-frame '(a b c) '(5 6 7) ) 
+               (cons (make-frame '(c d) '(110 12) ) '() ) ))))
+    (lexical-address-set! (make-lexical-address 2 1) env 11)
+    (check (equal? env  ( cons (make-frame '(a b ) '(1 2)) 
+         (cons (make-frame '(a b c) '(5 6 7) ) 
+               (cons (make-frame '(c d) '(110 11) ) '() ) ))) "should set the an element of a nested frame"))
+
     )
 
 (run-registered-tests)
