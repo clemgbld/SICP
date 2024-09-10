fdb1f8cf5079835cf22d15c7238cd8e3868f3add
cgombauld <clement.gombauld@cpexterne.org>
2024-09-10 09:14:50 +0200

    solve ex 49 of chapter 5

---
+++ ch-5/ex-5-49.scm
@@ -0,0 +1,116 @@
+(load "ch-5/ch5-regsim")
+(load "ch-5/ch5-eceval-support")
+(load "ch-5/ch5-compiler.scm")
+
+(define the-global-environment '())
+
+(define (start-eceval)
+  (set! the-global-environment (setup-environment))
+  (start eceval))
+
+(define (user-print object)
+  (cond ((compound-procedure? object)
+         (display (list 'compound-procedure
+                        (procedure-parameters object)
+                        (procedure-body object)
+                        '<procedure-env>)))
+        ((compiled-procedure? object)
+         (display '<compiled-procedure>))
+        (else (display object))))
+
+(define (compile-and-assemble expression)
+         (assemble (statements
+                    (compile expression 'val 'return))
+                   eceval))
+
+(define eceval-operations
+  (list
+   ;;primitive Scheme operations
+   (list 'read read)			;used by eceval
+
+   ;;used by compiled code
+   (list 'list list)
+   (list 'cons cons)
+
+   ;;operations in syntax.scm
+   (list 'self-evaluating? self-evaluating?)
+   (list 'quoted? quoted?)
+   (list 'text-of-quotation text-of-quotation)
+   (list 'variable? variable?)
+   (list 'assignment? assignment?)
+   (list 'assignment-variable assignment-variable)
+   (list 'assignment-value assignment-value)
+   (list 'definition? definition?)
+   (list 'definition-variable definition-variable)
+   (list 'definition-value definition-value)
+   (list 'lambda? lambda?)
+   (list 'lambda-parameters lambda-parameters)
+   (list 'lambda-body lambda-body)
+   (list 'if? if?)
+   (list 'if-predicate if-predicate)
+   (list 'if-consequent if-consequent)
+   (list 'if-alternative if-alternative)
+   (list 'begin? begin?)
+   (list 'begin-actions begin-actions)
+   (list 'last-exp? last-exp?)
+   (list 'first-exp first-exp)
+   (list 'rest-exps rest-exps)
+   (list 'application? application?)
+   (list 'operator operator)
+   (list 'operands operands)
+   (list 'no-operands? no-operands?)
+   (list 'first-operand first-operand)
+   (list 'rest-operands rest-operands)
+
+   ;;operations in eceval-support.scm
+   (list 'true? true?)
+   (list 'false? false?)		;for compiled code
+   (list 'make-procedure make-procedure)
+   (list 'compound-procedure? compound-procedure?)
+   (list 'procedure-parameters procedure-parameters)
+   (list 'procedure-body procedure-body)
+   (list 'procedure-environment procedure-environment)
+   (list 'extend-environment extend-environment)
+   (list 'lookup-variable-value lookup-variable-value)
+   (list 'set-variable-value! set-variable-value!)
+   (list 'define-variable! define-variable!)
+   (list 'primitive-procedure? primitive-procedure?)
+   (list 'apply-primitive-procedure apply-primitive-procedure)
+   (list 'prompt-for-input prompt-for-input)
+   (list 'announce-output announce-output)
+   (list 'user-print user-print)
+   (list 'empty-arglist empty-arglist)
+   (list 'adjoin-arg adjoin-arg)
+   (list 'last-operand? last-operand?)
+   (list 'no-more-exps? no-more-exps?)	;for non-tail-recursive machine
+   (list 'get-global-environment get-global-environment)
+
+   ;;for compiled code (also in eceval-support.scm)
+   (list 'make-compiled-procedure make-compiled-procedure)
+   (list 'compiled-procedure? compiled-procedure?)
+   (list 'compiled-procedure-entry compiled-procedure-entry)
+   (list 'compiled-procedure-env compiled-procedure-env)
+   (list  'compile-and-assemble compile-and-assemble)
+   ))
+
+(define eceval
+  (make-machine 
+    '(exp env val proc argl continue unev)
+   eceval-operations
+  '(
+read-eval-print-loop
+  (perform (op initialize-stack))
+  (perform
+   (op prompt-for-input) (const ";;; EC-Eval input:"))
+  (assign exp (op read))
+  (assign env (op get-global-environment))
+  (assign continue (label print-result))
+  (assign val (op compile-and-assemble) (reg exp))
+  (goto (reg val))
+print-result
+  (perform (op print-stack-statistics))
+  (perform
+   (op announce-output) (const ";;; EC-Eval value:"))
+  (perform (op user-print) (reg val))
+  (goto (label read-eval-print-loop))
+    ) ))
